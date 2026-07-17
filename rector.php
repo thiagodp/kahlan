463a0a909545d3955cf97655548cccff30791790
Abdul Malik Ikhsan <samsonasik@gmail.com>
2026-07-17 20:12:07 +0700

    Bump Rector to ^2.5 and re-run it (#445)
    * Bump Rector to ^2.5 and re-run it
    
    * skip TokenStream remove unused import, buggy on php 7.4

--- rector.php
+++ rector.php
@@ -3,9 +3,13 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
 
 return RectorConfig::configure()
     ->withPaths([__DIR__ . '/src'])
-    ->withSets([LevelSetList::UP_TO_PHP_74])
+    ->withSkip([
+        \Rector\PostRector\Rector\UnusedImportRemovingPostRector::class => [
+            __DIR__ . '/src/Jit/TokenStream.php'
+        ],
+    ])
+    ->withPhpSets()
     ->withImportNames();
